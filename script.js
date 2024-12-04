2d1cd08da398c1a421243b2b0590324dd3a11bc8
Pedro Henrique <pedropd.barbosa@gmail.com>
2024-12-04 12:56:15 -0300

    Finalizado renderização dos pots e iteração do post com a API

---
+++ script.js
@@ -0,0 +1,69 @@
+/* Implementando o pacote buscador */
+class PacoteBuscador {
+  constructor(baseURL) {
+    this.baseURL = baseURL;
+  }
+
+  get(endpoint) {
+    return fetch(this.baseURL + endpoint).then((response) => response.json());
+  }
+
+  put(endpoint, body) {
+    return this._send('put', endpoint, body);
+  }
+
+  post(endpoint, body) {
+    return this._send('post', endpoint, body);
+  }
+
+  delete(endpoint, body) {
+    return this._send('delete', endpoint, body);
+  }
+
+  _send(method, endpoint, body) {
+    return fetch(this.baseURL + endpoint, {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    }).then((response) => response.json());
+  }
+}
+
+const API = new PacoteBuscador('https://jsonplaceholder.typicode.com');
+const formPostagens = document.querySelector('#posts-form');
+const postsBody = document.querySelector('.posts-content-body');
+const formTitle = document.querySelector('.content-text #title');
+const formParagraph = document.querySelector('.content-text #paragraph');
+
+const obterPostagens = () => {
+  API.get('/posts').then((data) => {
+    const ultimasPostagens = data.slice(0, 9);
+
+    ultimasPostagens.forEach((postagem) => {
+      postsBody.insertAdjacentHTML(
+        'beforeend',
+        `
+          <div class="post-card">
+            <h2 class="title-card">${postagem.title.slice(0, 25)}</h2>
+            <p class="paragraph-card">${postagem.body}</p>
+          </div>
+        `
+      );
+    });
+  });
+};
+
+formPostagens.addEventListener('submit', (event) => {
+  event.preventDefault();
+  API.post('/posts', {
+    title: formTitle.value,
+    body: formParagraph.value,
+    userId: 1,
+  }).then((data) => {
+    console.log('Nova postagem adicionada:', data);
+  });
+});
+
+obterPostagens();
