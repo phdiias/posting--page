d22ae3b69c188c2e4a7842a03ebb7cf43a3479db
Pedro Henrique <pedropd.barbosa@gmail.com>
2024-12-04 14:15:03 -0300

    Finalizado a responsividade do site

--- script.js
+++ script.js
@@ -36,10 +36,12 @@ const formPostagens = document.querySelector('#posts-form');
 const postsBody = document.querySelector('.posts-content-body');
 const formTitle = document.querySelector('.content-text #title');
 const formParagraph = document.querySelector('.content-text #paragraph');
+const ancoras = document.querySelectorAll('.ancora');
+console.log(ancoras);
 
 const obterPostagens = () => {
   API.get('/posts').then((data) => {
-    const ultimasPostagens = data.slice(0, 9);
+    const ultimasPostagens = data.slice(0, 8);
 
     ultimasPostagens.forEach((postagem) => {
       postsBody.insertAdjacentHTML(
@@ -67,3 +69,25 @@ formPostagens.addEventListener('submit', (event) => {
 });
 
 obterPostagens();
+
+function getDistanceFromTheTop(element) {
+  const id = element.getAttribute('href');
+  return document.querySelector(id).offsetTop;
+}
+
+function nativeScroll(distanceFromTheTop) {
+  window.scroll({
+    top: distanceFromTheTop,
+    behavior: 'smooth',
+  });
+}
+
+function scrollToSection(event) {
+  event.preventDefault();
+  const distanceFromTheTop = getDistanceFromTheTop(event.target);
+  nativeScroll(distanceFromTheTop);
+}
+
+ancoras.forEach((link) => {
+  link.addEventListener('click', scrollToSection);
+});
